e6563be1871b6b37cc6d0a38c6609ee06488f212
wiktorwidera <wiktor.widera@gmail.com>
2023-05-07 14:15:00 +0200

    Adding a method that returns the remaining symbol from the given one

--- src/Symbol.java
+++ src/Symbol.java
@@ -2,4 +2,10 @@ import java.util.Random;
 public enum Symbol {
     O, X;
 
+    public static Symbol getAnotherSymbol(Symbol symbol) {
+        if (symbol.equals(Symbol.O)) {
+            return Symbol.X;
+        }
+        return Symbol.O;
+    }
 }
